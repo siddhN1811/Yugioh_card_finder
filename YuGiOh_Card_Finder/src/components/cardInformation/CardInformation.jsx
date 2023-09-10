e24603e6826e1ec1eb65c4151e008f3199aaaba1
siddhN1811 <siddhanthgn@yahoo.com>
2023-09-10 13:50:39 +0530

    pendulum effect and scale

--- YuGiOh_Card_Finder/src/components/cardInformation/CardInformation.jsx
+++ YuGiOh_Card_Finder/src/components/cardInformation/CardInformation.jsx
@@ -1,11 +1,9 @@
-
 import { useEffect, useState } from "react";
-import PropTypes from "prop-types"; 
+import PropTypes from "prop-types";
 import "./CardInformation.css";
 
 const CardInformation = ({ data }) => {
   const [carddata, setData] = useState(null);
-
   let atk = null;
   let desc = null;
   let type = null;
@@ -13,10 +11,12 @@ const CardInformation = ({ data }) => {
   let level = null;
   let def = null;
   let name = null;
+  let pendDesc = null;
+  let pendScale = null;
+
 
   if (data && data[0]) {
     const cardData = data[0];
-
     atk = cardData.atk;
     desc = cardData.desc;
     type = cardData.type;
@@ -24,31 +24,33 @@ const CardInformation = ({ data }) => {
     level = cardData.level;
     def = cardData.def;
     name = cardData.name;
+    pendDesc = cardData.pend_desc;
+    pendScale = cardData.scale;
+  
   }
 
-//   console.log("ATK:", atk);
-//   console.log("Description:", desc);
-//   console.log("Type:", type);
-//   console.log("Race:", race);
-//   console.log("Level:", level);
-//   console.log("Defense:", def);
-//   console.log("Name:", name);
+  
+  //   console.log("ATK:", atk);
+  //   console.log("Description:", desc);
+  //   console.log("Type:", type);
+  //   console.log("Race:", race);
+  //   console.log("Level:", level);
+  //   console.log("Defense:", def);
+  //   console.log("Name:", name);
 
   return (
     <div className="card-information-container">
       <div className="stats">
-        <li>Atk: {atk}</li>
-        <li>Def: {def}</li>
-        <li>Attribute: {race}</li>
-        <li>Type: {type}</li>
-        <li>Level: {level}</li>
+        <li>Atk: {" " + atk}</li>
+        <li>Def: {" " + def}</li>
+        <li>Attribute: {" " + race}</li>
+        <li>Type: {" " + type}</li>
+        <li>Level: {" " + level}</li>
+        {pendDesc!=null ? <li>Pendulum Scale:{" " + pendScale}</li> : null}
       </div>
-      <div className="name">
-        Name: {name}
-      </div>
-      <div className="desc">
-        Description: {desc}
-      </div>
+      <div className="name">Name: {name}</div>
+      
+      <div className="desc">{pendDesc!=null ? <li>Pendulum effect:{" " + pendDesc}</li> : null}<br></br> Description: {"  "+desc}</div>
     </div>
   );
 };
@@ -58,14 +60,15 @@ CardInformation.propTypes = {
     PropTypes.shape({
       atk: PropTypes.number,
       desc: PropTypes.string,
-      type: PropTypes.string, 
+      type: PropTypes.string,
       race: PropTypes.string,
       level: PropTypes.number,
       def: PropTypes.number,
       name: PropTypes.string,
+      pend_desc: PropTypes.string,
+      scale: PropTypes.number,
     })
   ),
 };
 
 export default CardInformation;
-
