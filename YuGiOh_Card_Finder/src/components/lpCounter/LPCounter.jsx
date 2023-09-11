4f3d760062b51c0417855fe5da5837e22c9347a3
siddhN1811 <siddhanthgn@yahoo.com>
2023-09-11 19:54:01 +0530

    bug fixes

--- YuGiOh_Card_Finder/src/components/lpCounter/LPCounter.jsx
+++ YuGiOh_Card_Finder/src/components/lpCounter/LPCounter.jsx
@@ -5,9 +5,9 @@ const LPCounter = () => {
   const [lpValue, setLpValue] = useState(8000);
   const [inputValue, setInputValue] = useState("");
 
-  const setLp = (e)=>{
-    setLpValue(e.target.value)
-  }
+  const setLp = (e) => {
+    setLpValue(e.target.value);
+  };
 
   const DecreaseLp = () => {
     setLpValue(lpValue - parseInt(inputValue));
@@ -16,27 +16,36 @@ const LPCounter = () => {
   const IncreaseLP = () => {
     setLpValue(lpValue + parseInt(inputValue));
   };
-
+  const mulLP = () => {
+    setLpValue(lpValue * parseInt(inputValue));
+  };
+  const divLP = () => {
+    setLpValue(lpValue / parseInt(inputValue));
+  };
   const TakeInput = (e) => {
-    setInputValue(e.target.value);
+    
+      setInputValue(parseInt(e.target.value));
+      console.log(typeof(inputValue));
+      console.log(typeof(lpValue))
+    
   };
 
   return (
     <div className="lp-counter-container">
-      <input
-        type="number"
-        value={lpValue}
-        onChange={setLp}
-      />
+      <input type="number" value={lpValue} onChange={setLp} />
 
       <div className="lp-changer">
+        <button onClick={mulLP}>*</button>
         <button onClick={IncreaseLP}>+</button>
+
         <input
           type="number"
           onChange={TakeInput}
           value={inputValue}
+          placeholder="0"
         />
         <button onClick={DecreaseLp}>-</button>
+        <button onClick={divLP}>/</button>
       </div>
       <button onClick={() => setLpValue(8000)}>Reset</button>
     </div>
